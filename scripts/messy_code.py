ec25856c58022065e314a4a3255989a3a4921404
victor <vn0027@princeton.edu>
2026-08-06 15:26:43 -0400

    improved messy code to count words in a text file

--- scripts/messy_code.py
+++ scripts/messy_code.py
@@ -30,25 +30,29 @@ def count_words_in_file(in_file, out_file):
 Smells:
 function doing more than one thing,
 not well documented
+uninformative variable names
 '''
 
 def count_words(text):
-    """Given a text file """
+    """Given a text file return a dictionary of counts of word occurences"""
     counts = {}
-    with open(text, 'r') as f:
+    with open(text, 'r') as file:
         for line in file:
             # Split words on spaces.
-            W = line.lower().split(' ')
-            for w in W:
-                if w != '':
-                    if w in counts:
-                        counts[w] += 1
-                    else:
-                        counts[w] = 0
+            words = line.lower().split()
+            for word in words:
+                if word in counts:
+                    counts[word] += 1
+                else:
+                    counts[word] = 0
     return counts
 
 def count_words_in_file(in_file, out_file):
     """Given a text file, count words in file """
+    count_words_dict = count_words(in_file)
     with open(out_file, 'w') as f:
-        for k in count_words(in_file).keys():
-            f.write(k + "," + str(count_words[k]) + "\n")
+        for k in count_words_dict.keys():
+            f.write(k + "," + str(count_words_dict[k]) + "\n")
+
+
+count_words_in_file("/Users/vn0027/Projects/pni-summerbootcamp/data/moby_dick.txt", "/Users/vn0027/Projects/pni-summerbootcamp/data/countwords_mobydick.txt")
